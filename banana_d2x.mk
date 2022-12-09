5cde7c57f0fae0c35a6424b3bb641da9d17835aa
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2022-12-09 08:16:39 -0500

    changed common_full_phone to common.mk

--- banana_d2x.mk
+++ banana_d2x.mk
@@ -37,7 +37,7 @@ TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/banana/config/common_full_phone.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
