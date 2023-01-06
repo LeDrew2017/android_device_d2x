0cad67d192c8ae5810adbf9225f2add90d34359f
LeDrew2017 <ledrew101806@gmail.com>
2023-01-06 10:07:16 -0500

    added elixir

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_d2x.mk
+    $(LOCAL_DIR)/elixir_d2x.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_d2x-eng \
-    xtended_d2x-user \
-    xtended_d2x-userdebug
+    elixir_d2x-eng \
+    elixir_d2x-user \
+    elixir_d2x-userdebug
