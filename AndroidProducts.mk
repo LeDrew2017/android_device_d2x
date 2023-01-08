69ca5c2f7a3e3447328348e48361e139f74989cc
LeDrew2017 <ledrew101806@gmail.com>
2023-01-07 21:30:21 -0500

    added bliss

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_d2x.mk
+    $(LOCAL_DIR)/bliss_d2x.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_d2x-eng \
-    xtended_d2x-user \
-    xtended_d2x-userdebug
+    bliss_d2x-eng \
+    bliss_d2x-user \
+    bliss_d2x-userdebug
